15315f446d6f0b624b15e1ca215d132d89981917
Jason Benn <jasoncbenn@gmail.com>
2019-03-26 21:15:56 -0700

    Rip out nginx.

--- devops/install-nginx-conf.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cp /home/jason/code/flock/devops/flock-nginx.conf /etc/nginx/sites-enabled/flock.conf
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+FLASK_DEBUG=1 FLASK_APP=server FLASK_ENV=development FLASK_RUN_PORT=8004 flask run
